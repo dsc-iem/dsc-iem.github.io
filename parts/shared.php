c15a42401dc484d5f00eac848267de21f901eb35
Asrient <asen182000@hotmail.com>
2019-12-31 16:44:29 +0530

    added navbar and footer to team page

---
+++ parts/shared.php
@@ -0,0 +1,22 @@
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="DSC IEM is a community which aims to upskills students and is powered by Google">
+    <title>DSC | IEM</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/assests/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/assests/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/assests/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/assests/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/assests/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/assests/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="/assests/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/css/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/themify/css/themify-icons.css">
+    <script type="text/javascript" src="/css/jquery/jquery.js"></script>
+    <!-- Font-Awesome CDN 🍏 -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Font Awesome CDN 🍎 -->
